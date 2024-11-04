bb47f02876489d6bc492aa7f54d1738b33f6f040
juan-rv <juanrodriguez9502@gmail.com>
2024-11-04 18:43:45 -0500

    agregando el frontend

---
+++ museo-evaluacion/frontend/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Este es el primer código de ejemplo y va ser la base para 
+          realizar el proyecto
+        </p>
+        <a>
+          Primer esquema de nuestra herramienta evaluativa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
